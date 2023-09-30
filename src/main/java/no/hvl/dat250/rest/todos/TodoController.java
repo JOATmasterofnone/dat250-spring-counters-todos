2e3caae8934bd48ab1fc53380e3cf5fb62e1eafd
iRW <raive4603@uib.no>
2023-09-30 21:49:33 +0200

    other changes

--- src/main/java/no/hvl/dat250/rest/todos/TodoController.java
+++ src/main/java/no/hvl/dat250/rest/todos/TodoController.java
@@ -1,9 +1,9 @@
 package no.hvl.dat250.rest.todos;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
+import java.util.NoSuchElementException;
+
 import java.util.HashMap;
 
 
@@ -13,18 +13,51 @@ import java.util.HashMap;
 public class TodoController {
 
   public static final String TODO_WITH_THE_ID_X_NOT_FOUND = "Todo with the id %s not found!";
-
-
   private static long counter;
-  private static HashMap<Todo, Long> toDoList = new HashMap<>();
+  private static HashMap<Long, Todo> toDoList = new HashMap<>();
 
 
   @PostMapping("/todos")
   public Todo createTodo(@RequestBody Todo requestedTodo) {
     Todo properTodo = new Todo(counter++, requestedTodo.getSummary(), requestedTodo.getDescription());
-    toDoList.put(properTodo, counter);
+    toDoList.put(counter, properTodo);
     return properTodo;
   }
 
+
+  @GetMapping("/todos/{id}")
+  public Todo readTodo(@PathVariable Long id) {
+    //Todo retrievedTodo;
+    Todo retrievedTodo = toDoList.get(id);
+    if (retrievedTodo == null) {
+      throw new NoSuchElementException(String.format(TODO_WITH_THE_ID_X_NOT_FOUND, id));
+    }
+    return retrievedTodo;
+  }
+
+
+  @PutMapping("/todos/{id}")
+  public Todo updateTodo(@PathVariable Long id, @RequestBody Todo requestedTodo) {
+    //Todo retrievedTodo;
+    Todo retrievedTodo = toDoList.get(id);
+    if (retrievedTodo == null) {
+      throw new NoSuchElementException(String.format(TODO_WITH_THE_ID_X_NOT_FOUND, id));
+    }
+
+    retrievedTodo.setDescription(requestedTodo.getDescription());
+    retrievedTodo.setSummary(requestedTodo.getSummary());
+    return requestedTodo;
+  }
+
+
+  @DeleteMapping("/todos/{id}")
+  public void deleteTodo(@PathVariable Long id) {
+    //Todo retrievedTodo;
+    Todo retrievedTodo = toDoList.get(id);
+    if (retrievedTodo == null) {
+      throw new NoSuchElementException(String.format(TODO_WITH_THE_ID_X_NOT_FOUND, id));
+    }
+    toDoList.remove(id);
+  }
 
 }
